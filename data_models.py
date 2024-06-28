2b47c247498d3b37ea946aeb0c91b791587a75b7
datta-abhi <rookbee91@gmail.com>
2024-06-28 17:19:34 +0530

    UpdateEmp Basemodel with optional args added

--- data_models.py
+++ data_models.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Emp(BaseModel):
     name: str
     age: int
     position: str
+    
+class UpdateEmp(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    position: Optional[str] = None    
