fb36034f48d9a9988bbbe1257c4d35394011a1dd
datta-abhi <rookbee91@gmail.com>
2024-06-28 17:20:25 +0530

    adding and updating employees using Pydantic BaseModel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from data_models import Emp,UpdateEmp
 
 # create instance of FASTAPI
 app = FastAPI(
@@ -31,14 +32,29 @@ def get_emp_info(emp_id:int):
     else:
         return employees[emp_id]
 
-@app.post('/add_employees')
-def add_employees(name: str,age: int,position: str):
+# adding new employee (POST)
+@app.post('/add-employee')
+def add_employees(emp:Emp):
     if not employees:
         new_id = 1
     else:
         new_id = max(employees.keys())+1         
-        employees[new_id]['name'] = name
-        employees[new_id]['age'] = age
-        employees[new_id]['position'] = position
-        
-        return employees[new_id]
+    employees[new_id] = emp.model_dump()
+                
+    return employees[new_id]
+
+# updating existing employee info
+@app.put('/update-employee/{emp_id}')
+def update_employee(emp_id:int, emp: UpdateEmp):
+    if emp_id not in employees:
+        return f"No employee found with employee ID {emp_id}"
+    else:
+        if emp.name is not None:
+            employees[emp_id]['name'] = emp.name
+        if emp.age is not None:
+            employees[emp_id]['age'] = emp.age
+        if emp.position is not None:
+            employees[emp_id]['position'] = emp.position
+            
+        return employees[emp_id]    # returns updated employee info
+                    
